ff20d2eb40984c6456c528a8293b6e495c985130
Jonathan M. Altman <jonathan@async.io>
2010-08-28 00:36:19 -0400

    If text/plain is first Accept: header match, returns converted string as plain text response

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#	 http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# The MIT License
+# 
+# Copyright (c) 2010 Jonathan M. Altman
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+__author__ = 'Jonathan M. Altman'
+
 import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -61,13 +70,22 @@ class MainHandler(webapp.RequestHandler):
 			output_text += output_sep + self.charDecoder[c.lower()]
 			if output_sep == '': output_sep = ' '
 
-		template_values = { 'app_title': app_title, 'input_text': input_text, 'output_text': output_text}
+		if self.request.accept.first_match(['text/html', 'text/plain']) == 'text/plain':
+			self.response.headers['Content-Type'] = 'text/plain'
+			self.response.out.write(output_text)
+			return
+		else:
+			pass
+		template_values = {
+			'app_title': app_title
+			, 'input_text': input_text
+			, 'output_text': output_text
+		}
 		path = os.path.join(os.path.dirname(__file__), 'templates/text2semaphore.html')
 		self.response.out.write(template.render(path, template_values))
 
 def main():
-	application = webapp.WSGIApplication([(r'/(\w*)', MainHandler)],
-										 debug=True)
+	application = webapp.WSGIApplication([(r'/(\w*)', MainHandler)], debug=False)
 	util.run_wsgi_app(application)
 
 
